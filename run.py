02c315e4b95e5bef4bf6c788ba543b75b79243b2
KarlTred265 Tred <tred26559@gmail.com>
2023-05-07 16:04:15 +0000

    Add more

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from random import randint
 
-# set up game board
-board = []
+# set up game boards
+player_board = []
+computer_board = []
 for x in range(0, 5):
-    board.append(["O"] * 5)
+    player_board.append(["O"] * 5)
+    computer_board.append(["O"] * 5)
 
-# place battleships
+# place battleships on computer board
 def place_battleships(board):
     for i in range(5):
         ship_row = randint(0, len(board) - 1)
@@ -17,8 +19,8 @@ def print_board(board):
     for row in board:
         print(" ".join(row))
 
-# initial placement of battleships
-place_battleships(board)
+# initial placement of battleships on computer board
+place_battleships(computer_board)
 
 # main game loop
 num_guesses = 0
@@ -26,6 +28,10 @@ num_battleships = 5
 playing = True
 
 while playing:
+    # print player board
+    print("Player board:")
+    print_board(player_board)
+    
     # get user guess
     guess_row = input("Guess Row (A-E): ")
     guess_col = input("Guess Col (1-5): ")
@@ -45,45 +51,45 @@ while playing:
     col = int(guess_col) - 1
     
     # check if user has already guessed that coordinate
-    if board[row][col] == "X":
+    if player_board[row][col] == "X":
         print("You already guessed that coordinate!")
         continue
     
     # check if user guessed a battleship
-    if board[row][col] == "B":
+    if computer_board[row][col] == "B":
         print("Congratulations! You sunk a battleship!")
-        board[row][col] = "X"
+        computer_board[row][col] = "X"
         num_battleships -= 1
+        player_board[row][col] = "X"
     else:
         print("You missed!")
-        board[row][col] = "X"
+        player_board[row][col] = "X"
     
     # check if user won
     if num_battleships == 0:
         print("Congratulations! You sunk all the battleships!")
         playing = False
     else:
-        print_board(board)
         num_guesses += 1
         
         # check if user has any guesses left
         if num_guesses == 10:
             print("Game over! You ran out of guesses.")
             playing = False
-            
+    
+    # print computer board
+    print("\nComputer board:")
+    computer_board_display = []
+    for i in range(len(computer_board)):
+        computer_board_display.append([])
+        for j in range(len(computer_board[0])):
+            if computer_board[i][j] == "B":
+                computer_board_display[i].append("O")
+            else:
+                computer_board_display[i].append(computer_board[i][j])
+    print_board(computer_board_display)
+    
 # ask user if they want to play again
 play_again = input("Do you want to play again? (y/n) ")
 while play_again.lower() not in ['y', 'n']:
-    play_again = input("Invalid input. Please enter 'y' for yes or 'n' for no. ")
-    
-if play_again.lower() == 'y':
-    board = []
-    for x in range(0, 5):
-        board.append(["O"] * 5)
-    place_battleships(board)
-    num_guesses = 0
-    num_battleships = 5
-    playing = True
-    print_board(board)
-else:
-    print("Thanks for playing! Goodbye.")
+    play_again = input("Invalid input. Please enter 'y' for")
