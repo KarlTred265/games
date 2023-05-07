96f08077ccc64ad0d7a5d2ba9912a2a99253ad95
KarlTred265 Tred <tred26559@gmail.com>
2023-05-07 11:56:42 +0000

    Add all

--- run.py
+++ run.py
@@ -1,68 +1,89 @@
-import random
+from random import randint
 
-def generate_board(size):
-    board = []
-    for row in range(size):
-        board.append(["O"] * size)
-    return board
+# set up game board
+board = []
+for x in range(0, 5):
+    board.append(["O"] * 5)
 
+# place battleships
+def place_battleships(board):
+    for i in range(5):
+        ship_row = randint(0, len(board) - 1)
+        ship_col = randint(0, len(board[0]) - 1)
+        board[ship_row][ship_col] = "B"
+        
+# print board function
 def print_board(board):
     for row in board:
         print(" ".join(row))
 
-def generate_ships(num_ships, board):
-    ships = []
-    for i in range(num_ships):
-        ship_row = random.randint(0, len(board) - 1)
-        ship_col = random.randint(0, len(board[0]) - 1)
-        while [ship_row, ship_col] in ships:
-            ship_row = random.randint(0, len(board) - 1)
-            ship_col = random.randint(0, len(board[0]) - 1)
-        ships.append([ship_row, ship_col])
-    return ships
+# initial placement of battleships
+place_battleships(board)
 
-def play_game(size, num_ships):
-    board = generate_board(size)
-    ships = generate_ships(num_ships, board)
-    print("Welcome to Battleships!")
-    print(f"You have {num_ships} ships to sink.")
-    print(f"Board size: {size}x{size}")
-    print_board(board)
-    for turn in range(10):
-        guess_row = int(input("Guess row (0 to {}): ".format(size - 1)))
-        guess_col = int(input("Guess col (0 to {}): ".format(size - 1)))
-        if [guess_row, guess_col] in ships:
-            print("Congratulations! You sank a battleship!")
-            board[guess_row][guess_col] = "X"
-            ships.remove([guess_row, guess_col])
+# main game loop
+num_guesses = 0
+num_battleships = 5
+playing = True
+
+while playing:
+    # get user guess
+    guess_row = input("Guess Row (A-E): ")
+    guess_col = input("Guess Col (1-5): ")
+    
+    # validate guess input
+    valid_input = False
+    while not valid_input:
+        if guess_row.upper() in ['A', 'B', 'C', 'D', 'E'] and guess_col in ['1', '2', '3', '4', '5']:
+            valid_input = True
         else:
-            if guess_row not in range(size) or \
-                guess_col not in range(size):
-                print("Oops, that's not even in the ocean.")
-            elif board[guess_row][guess_col] == "X":
-                print("You guessed that one already.")
-            else:
-                print("You missed the battleship!")
-                board[guess_row][guess_col] = "X"
-        print(f"Turn {turn + 1}:")
+            print("Invalid input. Please enter a letter A-E and a number 1-5.")
+            guess_row = input("Guess Row (A-E): ")
+            guess_col = input("Guess Col (1-5): ")
+            
+    # convert guess to board coordinates
+    row = ord(guess_row.upper()) - 65
+    col = int(guess_col) - 1
+    
+    # check if user has already guessed that coordinate
+    if board[row][col] == "X":
+        print("You already guessed that coordinate!")
+        continue
+    
+    # check if user guessed a battleship
+    if board[row][col] == "B":
+        print("Congratulations! You sunk a battleship!")
+        board[row][col] = "X"
+        num_battleships -= 1
+    else:
+        print("You missed!")
+        board[row][col] = "X"
+    
+    # check if user won
+    if num_battleships == 0:
+        print("Congratulations! You sunk all the battleships!")
+        playing = False
+    else:
         print_board(board)
-        if not ships:
-            print("Congratulations! You sank all the battleships!")
-            break
-    else:
-        print("Game over! You ran out of turns.")
-        print(f"The remaining battleships were at:")
-        for ship in ships:
-            print(f"({ship[0]}, {ship[1]})")
-
-def replay_game():
-    play_again = input("Do you want to play again? (y/n): ")
-    if play_again.lower() == "y":
-        play_game(size, num_ships)
-    else:
-        print("Thanks for playing!")
-
-size = 10
-num_ships = 5
-play_game(size, num_ships)
-replay_game()
+        num_guesses += 1
+        
+        # check if user has any guesses left
+        if num_guesses == 10:
+            print("Game over! You ran out of guesses.")
+            playing = False
+            
+# ask user if they want to play again
+play_again = input("Do you want to play again? (y/n) ")
+while play_again.lower() not in ['y', 'n']:
+    play_again = input("Invalid input. Please enter 'y' for yes or 'n' for no. ")
+    
+if play_again.lower() == 'y':
+    board = []
+    for x in range(0, 5):
+        board.append(["O"] * 5)
+    place_battleships(board)
+    num_guesses = 0
+    num_battleships = 5
+    playing = True
+    print_board(board)
+else:
+    print("Thanks for playing! Goodbye.")
